0c14fb17522d0de88924772cd4e99fff83dfb7c8
Rhodric McCurdy <rmccurdy268@gmail.com>
2025-12-03 01:51:10 -0700

    updated deploy script to login to ecr, ci saves secrets to env

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 IMAGE_URI="$1"
 
+REGION="${AWS_REGION}"
+ACCOUNT="${AWS_ACCOUNT}"
+
+echo "Logging in to ECR"
+aws ecr get-login-password --region "$REGION" \
+  | docker login --username AWS --password-stdin "$ACCOUNT.dkr.ecr.$REGION.amazonaws.com"
+
 echo "Pulling latest image: $IMAGE_URI"
-docker pull $IMAGE_URI
+docker pull "$IMAGE_URI"
 
 echo "Stopping old container..."
 docker stop habitapp || true
 docker rm habitapp || true
 
 echo "Starting new container..."
-docker run -d --name habitapp -p 80:3000 $IMAGE_URI
+docker run -d --name habitapp -p 80:3000 "$IMAGE_URI"
